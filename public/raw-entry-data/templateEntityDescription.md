88750906d69e7a38fe6b04c185a009aaf241798e
stswoon <xx@xx.xx>
2026-03-18 11:08:46 +0400

    template

---
+++ public/raw-entry-data/templateEntityDescription.md
@@ -0,0 +1,11 @@
+# alfagen
+
+Аналог **ChatGPT**
+
+# chatgpt
+
+ChatGPT is OpenAI’s large‑language‑model API providing text and multimodal generation capabilities.
+
+Generation capabilities:
+* text 
+* multimodal 
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -5,12 +5,13 @@ import type {RadarQuadrant} from "./types.ts";
  * чтобы точки всегда ставились одинаково при одном и том же id.
  */
 export function hashStringToUnit(str: string): number {
-  let hash = 0;
-  for (let i = 0; i < str.length; i++) {
-    hash = (hash * 31 + str.charCodeAt(i)) >>> 0;
-  }
-  const result = hash / 0xffffffff;
-  return result;
+    str = str + str;
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+        hash = (hash * 31 + str.charCodeAt(i)) >>> 0;
+    }
+    const result = hash / 0xffffffff;
+    return result;
 }
 
 export const quadrantsWithAnglesUtil = (quadrants: RadarQuadrant[]) => {
